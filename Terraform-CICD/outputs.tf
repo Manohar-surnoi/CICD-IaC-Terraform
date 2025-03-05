4938dadebf7fc2b10bcc2f407aadb0886531fd0f
Manohar-surnoi <manoharnandelli@surnoi.com>
2025-03-05 12:37:45 +0530

    changes made

--- Terraform-CICD/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "jenkins_ip" {
-  description = "The public IP of the Jenkins instance"
-  value       = module.jenkins.jenkins_public_ip
-}
-
-output "sonarqube_ip" {
-  description = "The public IP of the SonarQube instance"
-  value       = module.sonarqube.sonarqube_public_ip
-}
